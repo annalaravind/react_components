d49dff21efcaaac3657c36c310729bf72f54a341
annalaravind <annaldasaravind64@gmail.com>
2024-01-26 22:17:50 +0530

    Added CSS Properties

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 // import DropDown from './components/dropDowns/DropDown';
 // import Navbar from './components/navbar/Navbar';
 // import Carousel from './components/carousels/Carousel';
-import TextShadow from './components/textShadow/TextShadow';
+// import TextShadow from './components/textShadow/TextShadow';
+import CSS from './components/cssProperties/CSS';
 
 const App = () => {
   return (
@@ -12,7 +13,8 @@ const App = () => {
       {/* <DropDown /> */}
       {/* <Navbar /> */}
       {/* <Carousel /> */}
-      <TextShadow />
+      {/* <TextShadow /> */}
+      <CSS />
     </>
   );
 };
---
+++ src/components/cssProperties/CSS.jsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import './CSS.css';
+
+const CSS = () => {
+  return (
+    <>
+      <p>accent-color</p>
+      <div className="accent_color">
+        <input type="radio" />
+        <input type="checkbox" />
+      </div>
+      <p>align-content</p>
+      <div className="align_content">
+        <div className="align_content_property">
+          <div>A</div>
+          <div>B</div>
+          <div>C</div>
+          <div>D</div>
+          <div>E</div>
+          <div>F</div>
+        </div>
+      </div>
+      <p>align-items</p>
+      <div className="align_items">
+        <div className="align_items_property">
+          <div>A</div>
+          <div>A</div>
+          <div>A</div>
+        </div>
+      </div>
+      <p>align-self</p>
+      <div className="align_self">
+        <div className="align_self_property">
+          <div>A</div>
+          <div>A</div>
+          <div>A</div>
+        </div>
+      </div>
+      <p>all</p>
+      <div className="all">
+        <div className="all_property">
+          <div>Aravind</div>
+        </div>
+      </div>
+      <p>animation with all properties</p>
+      <div className="animation">
+        <div className="animation_all_properties">
+          <div></div>
+        </div>
+      </div>
+      <p>aspect-ratio</p>
+      <div className="aspect_ratio">
+        <div className="aspect_ratio_property">
+          <div>A</div>
+          <div>A</div>
+          <div>A</div>
+        </div>
+      </div>
+      <p>backdrop-filter</p>
+      <div className="backdrop_filter">
+        <div className="backdrop_filter_property">
+          <div>Aravind</div>
+        </div>
+      </div>
+      <p>backface-visibility</p>
+      <div className="backface_visibility">
+        <div className="backface_visibility_property">Aravind</div>
+      </div>
+      <p>background</p>
+      <div className="background">
+        <div className="background_all_properties">
+          <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+            blanditiis deserunt a, dolorum sunt architecto quisquam reiciendis,
+            neque minima est quam, ipsum voluptatibus? Praesentium
+            exercitationem obcaecati accusantium perferendis nam molestias!
+          </div>
+        </div>
+      </div>
+      <p>block-size</p>
+      <div className="block_size">
+        <div className="block_size_property">Aravind</div>
+      </div>
+    </>
+  );
+};
+
+export default CSS;
