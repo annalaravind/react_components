a90cc26af7dba730047f47a88c1caf1471d0e5c7
annalaravind <annaldasaravind64@gmail.com>
2024-01-27 22:23:28 +0530

    CSS some more properties

--- src/components/cssProperties/CSS.jsx
+++ src/components/cssProperties/CSS.jsx
@@ -81,6 +81,119 @@ const CSS = () => {
       <div className="block_size">
         <div className="block_size_property">Aravind</div>
       </div>
+      <p>border</p>
+      <div className="border">
+        <div className="border_property">Aravind</div>
+      </div>
+      <p>border-block</p>
+      <div className="border_block">
+        <div className="border_block_porperty">Aravind</div>
+        <div>
+          <p>border-block</p>
+          <p>border-block-start</p>
+          <p>border-block-end</p>
+        </div>
+      </div>
+      <p>border-bottom</p>
+      <div className="border_bottom">
+        <div className="border_bottom_property">Aravind</div>
+      </div>
+      <p>border-collapse</p>
+      <div className="border_collapse">
+        <table id="table">
+          <tr>
+            <th>Name</th>
+            <th>Class</th>
+            <th>Time</th>
+          </tr>
+          <tr>
+            <td>Aravind</td>
+            <td>I</td>
+            <td>09:30PM</td>
+          </tr>
+          <tr>
+            <td>PSPK</td>
+            <td>II</td>
+            <td>09:30PM</td>
+          </tr>
+        </table>
+      </div>
+      <p>block-end-end-radius</p>
+      <div className="block_end_end_radius">
+        <div className="block_end_end_radius_property">Aravind</div>
+        <div>
+          <p>
+            block-end-start-radius <br />
+            end-start means block-end and inline-start : : and other's like{' '}
+            <br />
+            block-start-start-radius <br />
+            block-start-end-radius
+          </p>
+        </div>
+      </div>
+      <p>border-image</p>
+      <div className="border_image">
+        <div className="border_image_property">Aravind</div>
+      </div>
+      <p>border-inline</p>
+      <div className="border_inline">
+        <div className="border_inline_porperty">Aravind</div>
+        <div>
+          <p>border-inline</p>
+          <p>border-inline-start</p>
+          <p>border-inline-end</p>
+        </div>
+      </div>
+      <p>border-left</p>
+      <div className="border_left">
+        <div className="border_left_property">Aravind</div>
+      </div>
+      <p>border-right</p>
+      <div className="border_right">
+        <div className="border_right_property">Aravind</div>
+      </div>
+      <p>border-radius</p>
+      <div className="border_radius">
+        <div className="border_radius_property">Aravind</div>
+      </div>
+      <p>border-spacing</p>
+      <div className="border_spacing">
+        <table id="table1">
+          <tr>
+            <th>Name</th>
+            <th>Class</th>
+            <th>Time</th>
+          </tr>
+          <tr>
+            <td>Aravind</td>
+            <td>I</td>
+            <td>09:30PM</td>
+          </tr>
+          <tr>
+            <td>PSPK</td>
+            <td>II</td>
+            <td>09:30PM</td>
+          </tr>
+        </table>
+      </div>
+      <p>bottom</p>
+      <div className="bottom">
+        <div className="bottom_property">
+          <div>Aravind</div>
+        </div>
+      </div>
+      <p>box-declaration-break</p>
+      <div className="box_declaration_break">
+        <div className="box_declaration_break_property">
+          <span id='span'>
+            Aravind <br />
+            is <br />
+            a <br />
+            good <br />
+            boy
+          </span>
+        </div>
+      </div>
     </>
   );
 };
